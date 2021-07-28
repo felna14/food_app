9a517f7b2d21c032dc67fecf0aeb60c2ae4e0771
felna14 <francyfelna14@gmail.com>
2021-07-28 19:52:58 +0530

    added

---
+++ food_project/foodiyoo/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+# Create your models here.
+from django.urls import reverse
+
+
+class Category(models.Model):
+    name=models.CharField(max_length=250,unique=True)
+    slug=models.CharField(max_length=250,unique=True)
+    image=models.ImageField(upload_to='category')
+
+    class Meta:
+        ordering=('name',)
+        verbose_name='category'
+        verbose_name_plural='categories'
+
+    def get_url(self):
+        return reverse("products_by_category", args=[self.slug])
+
+    def __str__(self):
+        return'{}'.format(self.name)
+
+class Product(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+    slug = models.CharField(max_length=250, unique=True)
+    image = models.ImageField(upload_to='product')
+    desc=models.TextField()
+    category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    price=models.DecimalField(max_digits=10,decimal_places=2)
+    stock=models.IntegerField()
+    available=models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
+
+    def get_url(self):
+        return reverse("ProdCatDetail", args=[self.category.slug, self.slug])
+
+    def __str__(self):
+        return '{}'.format(self.name)
